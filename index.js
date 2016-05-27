2e68e39c3968dba6ce0639d3ca0482953453c963
Meirion Hughes <mhughes@geologging.com>
2016-05-27 10:24:55 +0100

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var Linter = require('aurelia-template-lint').Linter;
+
+modules.exports = function (rules) {
+    
+    var linter = new Linter(rules);
+    
+    function sanitize(file, cb) {
+
+        var html = String(file.contents);
+
+        linter
+            .lint(html)
+            .then((errors) => {
+                errors.forEach((error) => {
+                    gutil.log('WARNING', error, 
+                    file.path.substring(file.cwd.length, file.path.Length));
+                });
+            })
+            .then(() => { cb(null, file) });
+    }
+    return require('event-stream').map(sanitize);
+}
