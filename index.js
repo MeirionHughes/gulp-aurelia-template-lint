2dc76d08b63376882353d022db9efae11192cb09
Meirion Hughes <mhughes@geologging.com>
2016-05-30 13:17:40 +0100

    update aurelia-template-lint

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var Linter = require('aurelia-template-lint').Linter;
+var Linter = require('template-lint').Linter;
+var SelfCloseRule = require('template-lint').SelfCloseRule;
+var ProjectionRule = require('aurelia-template-lint').ProjectionRule;
+var TemplateRule = require('aurelia-template-lint').TemplateRule;
+var RequireRule = require('aurelia-template-lint').RequireRule;
 
 module.exports = function (rules) {
-    
+
+    if (!rules)
+        rules = [
+            new SelfCloseRule(),
+            new ParserRule(), 
+            new ProjectionRule(), 
+            new TemplateRule(), 
+            new RequireRule()];
+
     var linter = new Linter(rules);
-    
+
     function sanitize(file, cb) {
 
         var html = String(file.contents);
@@ -14,8 +26,8 @@ module.exports = function (rules) {
             .lint(html)
             .then((errors) => {
                 errors.forEach((error) => {
-                    gutil.log('WARNING', error, 
-                    file.path.substring(file.cwd.length, file.path.Length));
+                    gutil.log('WARNING', error,
+                        file.path.substring(file.cwd.length, file.path.Length));
                 });
             })
             .then(() => { cb(null, file) });
